30b15cbdcdb063d87a77a4b6f64fb3f3642008c6
junqueiraagabi <junqueiragaabi10@gmail.com>
2022-12-25 17:58:35 -0300

    update colors and descriptions

--- R/butterfly_colors_function.R
+++ R/butterfly_colors_function.R
@@ -2,11 +2,10 @@
 butterfly_palettes <- list(
   philaethria_dido = c("#000000","#453E2D","#9EB5A2","#492B20","#4D4238"),
   anteos_menippe = c("#654118","#D38612","#C29821","#A8B999", "#000000"),
-  fountainea_ryphea = c(""),
-  lycorea_hallia = c(""),
-  morpho_helenor = c(""),
-  archaeoprepona_demophon_thalpius = c("")
-)
+  fountainea_ryphea = c("#3B1D1B", "#6D1F17", "#862735","#C1A58C","#B04320"),
+  lycorea_hallia = c("#B17E35", "#B1631C", "#BF9C4B", "#28150F","#BFC2BE"),
+  morpho_helenor = c("#A5C7D3", "#8FD1EE", "#3C8AAF", "#9BA2A5", "#15100D"),
+  archaeoprepona_demophon_thalpius = c("#2A2478", "#3EC1B8", "#9B6037", "#532C22", "#948777"))
 
 butterfly_pal <- function(species_butterfly){
 
@@ -15,12 +14,3 @@ butterfly_pal <- function(species_butterfly){
     stop("That species does not exist in the list ! :(")
   palette
 }
-
-
-butterfly_pal(species_butterfly = "philaethriaa")
-
-build_favicons(pkg = ".", overwrite = TRUE)
-
-pkgdown::build_favicons()
-
-init_site()
