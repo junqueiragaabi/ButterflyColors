dab8bbe7732411931e4b36c2560188b507c62320
junqueiragaabi <junqueiragaabi10@gmail.com>
2022-12-27 22:13:25 -0300

    update images

--- R/examples.R
+++ R/examples.R
@@ -7,12 +7,14 @@ print_pal(butterfly_palettes("fountainea_ryphea"))
 a <- ggplot(iris, aes(x = Sepal.Length, y = Petal.Length, colour = Species)) +
   geom_point(size = 3) +
   scale_colour_manual(values = butterfly_palettes("fountainea_ryphea")) + #1
-  theme_bw(base_size = 17)
+  theme_bw(base_size = 17) +
+  theme(legend.position = "bottom")
 
 b <- ggplot(diamonds, aes(x = cut, fill = cut)) +
   geom_bar() +
   scale_fill_manual(values = butterfly_palettes("fountainea_ryphea")) +
-  theme_bw(base_size = 17)
+  theme_bw(base_size = 17) +
+  theme(legend.position = "None")
 
 a + b
 
@@ -36,7 +38,8 @@ ggsave("anteos_menippe_plot.png")
 ggplot(iris, aes(x = Sepal.Length, y = Petal.Length, colour = Species)) +
   geom_point(size = 4) +
   scale_colour_manual(values = butterfly_palettes("parides_zacynthus_polymetus")) +
-  theme_bw(base_size = 17)
+  theme_bw(base_size = 17) +
+  theme(legend.position = "None")
 
 
 ggsave("parides_zacynthus_polymetus_plot.png")
